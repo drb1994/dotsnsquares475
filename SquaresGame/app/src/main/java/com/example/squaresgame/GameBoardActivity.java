8fde57110ca93eac86c60f73a222f9241b88a0b4
DESKTOP-7KAOKQJ\Justin <justinruffin19@yahoo.com>
2022-02-28 15:46:38 -0500

    Created basic UI design for game board screen

--- SquaresGame/app/src/main/java/com/example/squaresgame/GameBoardActivity.java
+++ SquaresGame/app/src/main/java/com/example/squaresgame/GameBoardActivity.java
@@ -1,2 +1,17 @@
-package com.example.squaresgame;public class GameBoardActivity {
+package com.example.squaresgame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+
+public class GameBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+        //some change
+    }
 }
