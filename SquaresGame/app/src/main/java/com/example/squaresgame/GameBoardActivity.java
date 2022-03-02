4d12087b502cee60b5bb5df5215006046d0ccff7
DESKTOP-7KAOKQJ\Justin <justinruffin19@yahoo.com>
2022-03-02 10:53:22 -0500

    Add color swap during moves

--- SquaresGame/app/src/main/java/com/example/squaresgame/GameBoardActivity.java
+++ SquaresGame/app/src/main/java/com/example/squaresgame/GameBoardActivity.java
@@ -12,14 +12,15 @@ import androidx.fragment.app.FragmentManager;
 
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GameBoardActivity extends AppCompatActivity {
-
     ImageButton undo_last_move, pause_button, tutorial_button;
     TextView player_one_score, player_two_score;
+    ImageView game_board;
 
-    Integer undo = 1;
+    Integer undo = 1, currentTurn = 0;
 
     Player playerOne, playerTwo;
 
@@ -38,6 +39,9 @@ public class GameBoardActivity extends AppCompatActivity {
         tutorial_button = findViewById(R.id.ib_tutorial_button);
         player_one_score = findViewById(R.id.tv_player_one_score);
         player_two_score = findViewById(R.id.tv_player_two_score);
+        game_board = findViewById(R.id.iv_game_board);
+
+        changeColor();
 
         // Undo button functionality
         undo_last_move.setOnClickListener(view -> {
@@ -46,6 +50,7 @@ public class GameBoardActivity extends AppCompatActivity {
             builder.setMessage("Are you sure you want to undo the last move?")
                     .setPositiveButton("OK", (dialogInterface, i) -> {
                         if(undo == 1){
+                            changeColor();
                             undo -= 1;
                         } else if(undo == 0){
                             androidx.appcompat.app.AlertDialog.Builder builder1 = new androidx.appcompat.app.AlertDialog.Builder(this);
@@ -68,5 +73,29 @@ public class GameBoardActivity extends AppCompatActivity {
             pauseMenu.show(fm, null);
         });
         //End of pause button functionality
+
+        game_board.setOnClickListener(view -> {
+            changeColor();
+            undo = 1;
+        });
+    }
+    public void changeColor(){
+        if(currentTurn == 0){
+            player_one_score.setBackgroundColor(getResources().getColor(playerOne.getColor()));
+            player_one_score.setTextColor(getResources().getColor(R.color.white));
+            player_two_score.setBackgroundColor(getResources().getColor(R.color.white));
+            player_two_score.setTextColor(getResources().getColor(R.color.black));
+        }
+        if(currentTurn == 1){
+            player_one_score.setBackgroundColor(getResources().getColor(R.color.white));
+            player_one_score.setTextColor(getResources().getColor(R.color.black));
+            player_two_score.setBackgroundColor(getResources().getColor(playerTwo.getColor()));
+            player_two_score.setTextColor(getResources().getColor(R.color.white));
+        }
+        if(currentTurn == 0){
+            currentTurn = 1;
+        } else {
+            currentTurn = 0;
+        }
     }
 }
